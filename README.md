24f39086a8d13fec5e3667f74ccf59e55027da53
luoyingjack <511794867@qq.com>
2019-09-11 11:20:35 +0800

    增加发送邮件案例

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flask-jenkins-demo
+# code_life
 
 ## Environment Variables
 
--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,11 @@
+import email
 from os import getenv
 
 from huey import RedisHuey
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
 
 
 huey = RedisHuey(
@@ -9,3 +14,47 @@ huey = RedisHuey(
     port=int(getenv('REDIS_PORT') or 6379),
     db=int(getenv('REDIS_DB') or 0)
 )
+
+
+@huey.task()  # 发送邮件案例
+def send_email(content, name, phone, e_mail):
+    """发送邮件"""
+    try:
+        username = 'metapan@dm.interval.im'  # 发送人邮箱
+        password = '13lsnXORVyokNpI'  # 发送密钥
+        replyto = ''  # 回复邮件接受人邮箱
+        receiver = 'info@tailai.bio'  # 接受者邮箱
+        msg = MIMEMultipart('html')
+        msg['Subject'] = Header('邮件标题')
+        from_header = Header('metapan@dm.interval.im', 'utf-8')  # 来自...
+        from_header.append('<{}>'.format(username), 'ascii')
+        msg['From'] = from_header
+        msg['To'] = receiver
+        msg['Reply-to'] = replyto
+        msg['Message-id'] = email.utils.make_msgid()
+        msg['Date'] = email.utils.formatdate()
+        content = "邮件内容：" \
+                  "<br>" \
+                  "<br>" \
+                  "<br>" \
+                  "{0}" \
+                  "<br>" \
+                  "<br>" \
+                  "<br>" \
+                  "来自：{1}" \
+                  "<br>" \
+                  "联系电话：{2}" \
+                  "<br>" \
+                  "邮箱地址：{3}".format(content, name, phone, e_mail)
+        html = MIMEText(content, _subtype='html', _charset='UTF-8')
+        msg.attach(html)
+        try:
+            client = smtplib.SMTP()
+            client.connect('smtpdm.aliyun.com', 80)
+            client.login(username, password)
+            client.sendmail(username, receiver, msg.as_string())
+            client.quit()
+        except Exception as e:
+            print('1020', e, flush=True)
+    except Exception as e:
+        print('1022', e, flush=True)
